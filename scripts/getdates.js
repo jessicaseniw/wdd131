52928ed5d7632743f0e0ac386de77f2d2fe728e8
jessicaseniw <jseniwborges@byupathway.edu>
2026-01-10 11:53:07 -0300

    creating home page

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Current year
+const currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear;
+
+// Last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
