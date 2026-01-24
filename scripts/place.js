0e615f33e7bcaf05072a7a2130f477084098da6a
jessicaseniw <jseniwborges@byupathway.edu>
2026-01-24 09:21:16 -0300

    adding images

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,6 @@
+// Current year
+document.getElementById("year").textContent = new Date().getFullYear();
+
 // Last modified date
 document.getElementById("lastModified").textContent = document.lastModified;
 
